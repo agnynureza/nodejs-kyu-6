84d939fb9e2ad7d9042a6aa5791f974ae33022aa
agnynureza <agny.nureza@amarbank.co.id>
2019-11-07 14:29:12 +0700

    shooping-time

---
+++ shooping-time.js
@@ -0,0 +1,26 @@
+function shoppingTime(memberId, money){
+    let tokoX = [
+        ['Sepatu Stacattu',1500000],
+        ['Baju Zoro', 500000],
+        ['Baju H&N',250000],
+        ['Sweater Uniklooh',175000],
+        ['Casing Handphone', 50000]
+    ]
+    let sisa = money
+
+}
+
+console.log(shoppingTime('1820RzKrnWn08', 2475000))
+  //{ memberId: '1820RzKrnWn08',
+  // money: 2475000,
+  // listPurchased:
+  //  [ 'Sepatu Stacattu',
+  //    'Baju Zoro',
+  //    'Baju H&N',
+  //    'Sweater Uniklooh',
+  //    'Casing Handphone' ],
+  // changeMoney: 0 }
+console.log(shoppingTime('82Ku8Ma742', 170000))
+console.log(shoppingTime('', 2475000))
+console.log(shoppingTime('234JdhweRxa53', 15000))
+console.log(shoppingTime(''))
