e83c509abc78ceb7055a44132395504c4eeb72f9
agnynureza <agny.nureza@amarbank.co.id>
2019-11-10 14:42:48 +0700

    graduate object

---
+++ graduate.js
@@ -0,0 +1,96 @@
+function graduates (students) {
+    let obj = {}
+    let min = 75
+    for(let i = 0 ; i < students.length ; i++){
+        let exists = Object.keys(obj)
+        if(exists.indexOf(students[i].class) != -1) continue
+        let temp = []
+        for(let j = 0 ; j < students.length ; j++){
+            if(students[j].score > min && students[i].class == students[j].class){
+                temp.push({
+                    "name": students[j].name,
+                    "score" : students[j].score
+                })
+            }
+        }
+        obj[students[i].class] = temp    
+    }
+    return obj
+}
+  
+  console.log(graduates([
+    {
+      name: 'Dimitri',
+      score: 90,
+      class: 'foxes'
+    },
+    {
+      name: 'Alexei',
+      score: 85,
+      class: 'wolves'
+    },
+    {
+      name: 'Sergei',
+      score: 74,
+      class: 'foxes'
+    },
+    {
+      name: 'Anastasia',
+      score: 78,
+      class: 'wolves'
+    }
+  ]));
+  
+  // {
+  //   foxes: [
+  //     { name: 'Dimitri', score: 90 }
+  //   ],
+  //   wolves: [
+  //     { name: 'Alexei' , score: 85 },
+  //     { name: 'Anastasia', score: 78 }
+  //   ]
+  // }
+  
+  console.log(graduates([
+    {
+      name: 'Alexander',
+      score: 100,
+      class: 'foxes'
+    },
+    {
+      name: 'Alisa',
+      score: 76,
+      class: 'wolves'
+    },
+    {
+      name: 'Vladimir',
+      score: 92,
+      class: 'foxes'
+    },
+    {
+      name: 'Albert',
+      score: 71,
+      class: 'wolves'
+    },
+    {
+      name: 'Viktor',
+      score: 80,
+      class: 'tigers'
+    }
+  ]));
+  
+  // {
+  //   foxes: [
+  //     { name: 'Alexander', score: 100 },
+  //     { name: 'Vladimir', score: 92 }
+  //   ],
+  //   wolves: [
+  //     { name: 'Alisa', score: 76 },
+  //   ],
+  //   tigers: [
+  //     { name: 'Viktor', score: 80 }
+  //   ]
+  // }
+  
+  
+  console.log(graduates([])); //{}
