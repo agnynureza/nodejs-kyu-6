2b827f36b3440dfdc3369f4b7271bce313ad8583
agnynureza <agny.nureza@amarbank.co.id>
2020-05-21 13:42:37 +0700

    buble_sort

--- Node.js/sorting/bubble_sort.js
+++ Node.js/sorting/bubble_sort.js
@@ -14,8 +14,8 @@ function swap(arr,index_1,index_2){
 function bubble_sort(arr){
     for(let i = 0 ; i < arr.length ; i ++){
         for(let j = 0 ; j < arr.length-i-1; j++){
-            if(arr[j] > arr[j+1]){
-                arr = swap(arr,j, j+1)
+            if(arr[j] < arr[j+1]){
+                swap(arr,j, j+1)
             }
         }
     }
